24e780e8850bb51e3176f3dc1892951d8cb96bbf
xrouterservice <xrouterservice@gmail.com>
2020-02-19 20:36:04 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,7 +7,7 @@
 # https://github.com/RealKiro/Actions-OpenWrt/blob/r619ac/diy.sh
 #=================================================
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 
 # 更改默认主题为Argon
 # sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' package/feeds/luci/luci/Makefile
