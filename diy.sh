051d060ca22659ba8a8c6b99ed6d9a46133cdf68
xrouterservice <xrouterservice@gmail.com>
2020-03-10 12:50:55 +0800

    Update diy.sh
    dnspod luci 不更新

--- diy.sh
+++ diy.sh
@@ -76,6 +76,7 @@ git clone https://github.com/vernesong/OpenClash.git package/mine/OpenClash
 
 #luci-app-dnspod
 # git clone https://github.com/bdhjiajia/luci-app-dnspod.git package/mine/luci-app-dnspod
+# git clone https://github.com/ntlf9t/luci-app-dnspod.git package/mine/luci-app-dnspod
 
 #luci-app-vssr
 #git clone https://github.com/Leo-Jo/luci-app-vssr.git package/mine/luci-app-vssr
